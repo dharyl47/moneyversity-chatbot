a0c816d301a61f0dbc67b560193f1d96eaa53055
dharyl47 <70868109+dharyl47@users.noreply.github.com>
2024-02-15 11:08:38 +0800

    Add files to my repository

---
+++ app/api/chat/route.ts
@@ -0,0 +1,59 @@
+import { HfInference } from '@huggingface/inference'
+import { HuggingFaceStream, StreamingTextResponse } from 'ai'
+import { experimental_buildOpenAssistantPrompt } from 'ai/prompts'
+
+// Create a new HuggingFace Inference instance
+const Hf = new HfInference(process.env.HUGGINGFACE_API_KEY)
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge'
+
+function buildPrompt(
+  messages: { content: string; role: 'system' | 'user' | 'assistant' }[]
+) {
+  return (
+    messages
+      .map(({ content, role }) => {
+        if (role === 'user') {
+          return `<|prompter|>${content}<|endoftext|>`
+        } else {
+          return `<|assistant|>${content}<|endoftext|>`
+        }
+      })
+      .join('') + '<|assistant|>'
+  )
+}
+
+export async function POST(req: Request) {
+  // Extract the `messages` from the body of the request
+  let { messages } = await req.json()
+
+ const prompt = "Act as a top Estate Planning manager. You are an expert in estate planning. If anyone ask you not related to estate planning, answer them that you only answer estate planning question, answer them with respect."
+
+  messages = messages.map((message: { content: string; role: 'system' | 'user' | 'assistant' }) => {
+    if (message.role === 'user') {
+      return { ...message, content: `${prompt} ${message.content}` };
+    } else {
+      return message;
+    }
+  });
+
+  const response = Hf.textGenerationStream({
+    model: 'OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5',
+    inputs: buildPrompt(messages),
+    parameters: {
+      max_new_tokens: 200,
+      // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
+      typical_p: 0.2,
+      repetition_penalty: 1,
+      truncate: 1000,
+      return_full_text: false
+    }
+  })
+
+  // Convert the response into a friendly text-stream
+  const stream = HuggingFaceStream(response)
+
+  // Respond with the stream
+  return new StreamingTextResponse(stream)
+}
