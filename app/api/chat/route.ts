5995b930a9267649b9453e75368fdb4427298670
dharyl47 <70868109+dharyl47@users.noreply.github.com>
2024-02-15 11:33:00 +0800

    added

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -39,7 +39,7 @@ export async function POST(req: Request) {
   });
 
   const response = Hf.textGenerationStream({
-    model: 'OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5',
+    model: 'meta-llama/Llama-2-70b-chat-hf',
     inputs: buildPrompt(messages),
     parameters: {
       max_new_tokens: 200,
